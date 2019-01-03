11d7d92b31e05676833ea3c3ef8a85b2ac254071
Michal Kwiatek <kwiatekf@onet.pl>
2019-01-03 15:16:42 +0100

    Add not available restaurants

--- frontend/src/components/CheckoutPage/index.js
+++ frontend/src/components/CheckoutPage/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import BaseComponent from '../BaseComponent';
 import { withRouter } from "react-router";
 import RestaurantAvailableForCheckout from '../RestaurantAvailableForCheckout';
+import RestaurantNotAvailableForCheckout from '../RestaurantNotAvailableForCheckout';
+import './styles.css'
 
 class CheckoutPage extends BaseComponent {
 
   render() {
     return (
-      <div>
+      <div className='checkoutPage'>
         <RestaurantAvailableForCheckout checkoutId={this.props.match.params.id} />
+        <RestaurantNotAvailableForCheckout checkoutId={this.props.match.params.id} />
       </div>
     );
   }
