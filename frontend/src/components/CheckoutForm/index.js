4701ee6eb8d4b9ae8ee6eff44f05a4798ca45404
Michal Kwiatek <kwiatekf@onet.pl>
2019-01-03 14:13:26 +0100

    Add cheeckout with correct date

--- frontend/src/components/CheckoutForm/index.js
+++ frontend/src/components/CheckoutForm/index.js
@@ -7,20 +7,25 @@ import * as alert from '../../utils/altert'
 
 class CheckoutsForm extends BaseComponent {
 
+  state = {
+    timestamp: Date.now(),
+  }
+
   render() {
     return (
       <div>
-        <Mutation 
-          mutation={CREATE_CHECKOUT} 
-          variables={{ date: `${Date.now()}`, status: 'ACTIVE' }}
+        <Mutation
+          mutation={CREATE_CHECKOUT}
+          variables={{ date: `${this.state.timestamp}`, status: 'ACTIVE' }}
           onError={error => alert.error(error.message)}
           onCompleted={() => {
             alert.success('Checkout created!')
+            this.setState({ timestamp: Date.now() })
           }}
         >
           {onCreateCheckout => <Button color="success" onClick={onCreateCheckout}>{'Add new checkout'}</Button>}
         </Mutation>
-        
+
       </div>
     )
   }
